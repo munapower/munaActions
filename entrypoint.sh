be696fbf7ba469387287165e84aafb3173711db5
munapower <mmunaro@hotmail.com>
2024-01-26 13:41:30 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ set -e
     DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
     DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
     DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
-    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
-    echo $DATA | http POST $URL | jq .
+    URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
+    echo $DATA | http -H "'Authorization: token ${GITHUB_TOKEN}'" POST $URL | jq .
 
