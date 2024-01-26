e4cf075a10ab2c3de54626f40aa39d1ecb65d953
munapower <mmunaro@hotmail.com>
2024-01-26 20:13:34 -0300

    Print headers

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,6 @@ http --version
     DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
     DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
-    echo $DATA | JWT_AUTH_PREFIX=token http -A bearer -a "${GITHUB_TOKEN}" POST $URL -h | jq .
+    echo $DATA | JWT_AUTH_PREFIX=token http -A bearer -a "${GITHUB_TOKEN}" POST $URL -h --print=Hh
+    #| jq '.'
 
